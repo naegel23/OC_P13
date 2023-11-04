6b847b65611307aae0c8d7cc0c2c04066d2830ea
naegel <elcabiri.najim@gmail.com>
2023-11-04 21:26:32 +0100

    add tests

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 sentry_sdk.init(
